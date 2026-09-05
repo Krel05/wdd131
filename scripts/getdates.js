a9771a91963567efe61b641f616ce298b78b00a0
Krel05 <jmenasantes@byupathway.edu>
2026-09-04 20:09:10 -0600

    added span element in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,5 @@ const today = new Date();
 
 document.getElementById("lastModified").textContent = `Last modification: ${document.lastModified}`;
 
-currentyear.innerHTML = `&copy <span>${today.getFullYear()}</span> | Jorge Gabriel Mena Santes | Mexico City`;
+currentyear.innerHTML = `${today.getFullYear()}`;
 
